065ac11e21513342510c45499a56473e8e8475e7
Eric Malachias <malachias.eric@gmail.com>
2017-06-10 18:34:00 -0300

    [api] add manager and admin to UserTableSeeder

--- jogging-api/database/seeds/UserTableSeeder.php
+++ jogging-api/database/seeds/UserTableSeeder.php
@@ -20,6 +20,20 @@ class UserTableSeeder extends Seeder
             factory(Jog::class, 50)->create([
                 'owner_id' => $user->id,
             ]);
+
+            $manager = factory(User::class)->create([
+                'name' => 'Manager',
+                'email' => 'manager@example.com',
+                'password' => '123456',
+                'role_id' => app(RoleRepository::class)->findByNameOrFail('manager')->id
+            ]);
+
+            $admin = factory(User::class)->create([
+                'name' => 'Admin',
+                'email' => 'admin@example.com',
+                'password' => '123456',
+                'role_id' => app(RoleRepository::class)->findByNameOrFail('admin')->id
+            ]);
         });
     }
 }
