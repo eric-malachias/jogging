3a88ddaccd5ceaf95f7056bca6aa8d50f5976998
Eric Malachias <malachias.eric@gmail.com>
2017-06-11 00:10:25 -0300

    [web] refactor jog list into a different file

--- jogging-web/src/pages/JogListPage.vue
+++ jogging-web/src/pages/JogListPage.vue
@@ -35,85 +35,36 @@
                 </div>
             </div>
         </div>
-        <div class="jogs">
-            <div class="wrapper">
-                <alert v-if="success" type="success" dismissable="true" :content="t('success.jog.removed')" @dismiss="success = false" timer="5"></alert>
-                <alert v-if="error" type="danger" dismissable="true" :content="t(`error.jogs.${error.status}`)" @dismiss="error = ''"></alert>
-            </div>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>{{ t('startedAt') }}</th>
-                        <th>{{ t('distance') }}</th>
-                        <th>{{ t('duration') }}</th>
-                        <th>{{ t('speed') }}</th>
-                        <th>{{ t('actions') }}</th>
-                    </tr>
-                </thead>
-                <tbody v-if="pagination.total > 0">
-                    <tr v-for="(jog, index) in jogs" :key="jog.id">
-                        <td>{{ jog.started_at | date }}</td>
-                        <td>{{ jog.distance | meters }}</td>
-                        <td>{{ calculateDuration(jog) | minutes }}</td>
-                        <td>{{ calculateSpeed(jog) | speed }}</td>
-                        <td>
-                            <button class="btn btn-primary btn-xs" :title="t('hint.editJog')" @click="editJog(jog)"><span class="glyphicon glyphicon-pencil"></span></button>
-                            <button class="btn btn-danger btn-xs" :title="t('hint.removeJog')" @click="removeJog(jog, index)"><span class="glyphicon glyphicon-remove"></span></button>
-                        </td>
-                    </tr>
-                </tbody>
-                <tbody v-else class="no-jogs text-center">
-                    <tr v-if="!pagination.total">
-                        <td colspan="5">{{ t('noJogs') }}</td>
-                    </tr>
-                </tbody>
-            </table>
-            <div class="pagination-container text-center" v-if="pagination.last_page > 1">
-                <pagination :pagination="pagination" :callback="loadJogs" :options="paginationOptions"></pagination>
-            </div>
-        </div>
-        <jog-weekly-report :refresh-token="refreshToken"></jog-weekly-report>
+        <jog-list :filters="filters" :refresh-token="refresh.list"></jog-list>
+        <jog-weekly-report :refresh-token="refresh.report"></jog-weekly-report>
     </div>
 </template>
 
 <script>
-import Auth from '@/services/Auth'
-import Http from '@/services/Http'
-import Pagination from 'vue-bootstrap-pagination'
-import Jog from '@/services/Jog'
 import DateTimePicker from '@/components/DateTimePicker'
 import _ from 'lodash'
-import Alert from '@/components/Alert'
 import JogWeeklyReport from './JogListPage/JogWeeklyReport'
+import JogList from './JogListPage/JogList'
 
 export default {
     components: {
-        Pagination,
         DateTimePicker,
-        Alert,
-        JogWeeklyReport
+        JogWeeklyReport,
+        JogList
     },
     data () {
         return {
-            error: '',
             filters: {
                 from: '',
                 to: ''
             },
-            jogs: [],
-            pagination: {},
-            paginationOptions: {
-                offset: 1,
-                alwaysShowPrevNext: true
-            },
             showFilters: false,
-            success: false,
-            refreshToken: 0,
+            refresh: {
+                list: 0,
+                rerport: 0
+            },
             _loadJogs: null
         }
-    },
-    created () {
-        this.loadJogs()
     },
     watch: {
         'filters.from' () {
@@ -134,36 +85,8 @@ export default {
 
             return this.applyFilters()
         },
-        calculateDuration (jog) {
-            if (jog.duration) {
-                return jog.duration
-            }
-
-            jog.duration = Jog.calculateDuration(jog.started_at, jog.ended_at)
-
-            return jog.duration
-        },
-        calculateSpeed (jog) {
-            if (jog.speed) {
-                return jog.speed
-            }
-
-            const distance = jog.distance
-            const duration = this.calculateDuration(jog)
-
-            jog.speed = Jog.calculateSpeed(distance, duration)
-
-            return jog.speed
-        },
-        clearAlerts () {
-            this.error = ''
-            this.success = false
-        },
         createJog () {
             this.$router.push('/jogs/new')
-        },
-        editJog (jog) {
-            this.$router.push(`/jogs/${jog.id}`)
         },
         getFilterCount () {
             let count = 0
@@ -174,35 +97,8 @@ export default {
 
             return count
         },
-        loadJogs (page) {
-            Http
-                .get(this, `users/${Auth.user.id}/jogs`, {
-                    params: {
-                        page: page || this.pagination.current_page || 1,
-                        before: this.filters.to,
-                        after: this.filters.from
-                    }
-                })
-                .then(response => {
-                    this.jogs = response.body.data
-                    this.pagination = response.body
-                })
-        },
-        removeJog (jog, index) {
-            this.clearAlerts()
-
-            Http
-                .delete(this, `jogs/${jog.id}`)
-                .then(response => {
-                    this.refreshToken++
-                    this.success = true
-                    this.jogs.splice(index, 1)
-                    this.loadJogs(
-                        this.jogs.length > 0
-                            ? this.pagination.current_page
-                            : (this.pagination.current_page - 1) || 1
-                    )
-                })
+        loadJogs () {
+            this.refresh.list++
         }
     }
 }
@@ -221,8 +117,5 @@ export default {
     }
     .main-actions {
         padding: 15px 0;
-    }
-    table {
-        table-layout: fixed;
     }
 </style>
---
+++ jogging-web/src/pages/JogListPage/JogList.vue
@@ -0,0 +1,141 @@
+<template>
+    <div class="jog-list">
+        <div class="wrapper">
+            <alert v-if="success" type="success" dismissable="true" :content="t('success.jog.removed')" @dismiss="success = false" timer="5"></alert>
+            <alert v-if="error" type="danger" dismissable="true" :content="t(`error.jogs.${error.status}`)" @dismiss="error = ''"></alert>
+        </div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>{{ t('startedAt') }}</th>
+                    <th>{{ t('distance') }}</th>
+                    <th>{{ t('duration') }}</th>
+                    <th>{{ t('speed') }}</th>
+                    <th>{{ t('actions') }}</th>
+                </tr>
+            </thead>
+            <tbody v-if="pagination.total > 0">
+                <tr v-for="(jog, index) in jogs" :key="jog.id">
+                    <td>{{ jog.started_at | date }}</td>
+                    <td>{{ jog.distance | meters }}</td>
+                    <td>{{ calculateDuration(jog) | minutes }}</td>
+                    <td>{{ calculateSpeed(jog) | speed }}</td>
+                    <td>
+                        <button class="btn btn-primary btn-xs" :title="t('hint.editJog')" @click="edit(jog)"><span class="glyphicon glyphicon-pencil"></span></button>
+                        <button class="btn btn-danger btn-xs" :title="t('hint.removeJog')" @click="remove(jog, index)"><span class="glyphicon glyphicon-remove"></span></button>
+                    </td>
+                </tr>
+            </tbody>
+            <tbody v-else class="no-jogs text-center">
+                <tr v-if="!pagination.total">
+                    <td colspan="5">{{ t('noJogs') }}</td>
+                </tr>
+            </tbody>
+        </table>
+        <div class="pagination-container text-center" v-if="pagination.last_page > 1">
+            <pagination :pagination="pagination" :callback="load" :options="paginationOptions"></pagination>
+        </div>
+    </div>
+</template>
+
+<script>
+import Alert from '@/components/Alert'
+import Auth from '@/services/Auth'
+import Http from '@/services/Http'
+import Jog from '@/services/Jog'
+import Pagination from 'vue-bootstrap-pagination'
+
+export default {
+    components: {
+        Alert,
+        Pagination
+    },
+    props: ['filters', 'refresh-token'],
+    data () {
+        return {
+            error: '',
+            jogs: [],
+            pagination: {},
+            paginationOptions: {
+                offset: 1,
+                alwaysShowPrevNext: true
+            },
+            success: false
+        }
+    },
+    created () {
+        this.load()
+    },
+    watch: {
+        refreshToken () {
+            this.load()
+        }
+    },
+    methods: {
+        calculateDuration (jog) {
+            if (jog.duration) {
+                return jog.duration
+            }
+
+            jog.duration = Jog.calculateDuration(jog.started_at, jog.ended_at)
+
+            return jog.duration
+        },
+        calculateSpeed (jog) {
+            if (jog.speed) {
+                return jog.speed
+            }
+
+            const distance = jog.distance
+            const duration = this.calculateDuration(jog)
+
+            jog.speed = Jog.calculateSpeed(distance, duration)
+
+            return jog.speed
+        },
+        clearAlerts () {
+            this.error = ''
+            this.success = false
+        },
+        edit (jog) {
+            this.$router.push(`/jogs/${jog.id}`)
+        },
+        load (page) {
+            Http
+                .get(this, `users/${Auth.user.id}/jogs`, {
+                    params: {
+                        page: page || this.pagination.current_page || 1,
+                        before: this.filters.to,
+                        after: this.filters.from
+                    }
+                })
+                .then(response => {
+                    this.jogs = response.body.data
+                    this.pagination = response.body
+                })
+        },
+        remove (jog, index) {
+            this.clearAlerts()
+
+            Http
+                .delete(this, `jogs/${jog.id}`)
+                .then(response => {
+                    this.refreshToken++
+                    this.success = true
+                    this.jogs.splice(index, 1)
+                    this.load(
+                        this.jogs.length > 0
+                            ? this.pagination.current_page
+                            : (this.pagination.current_page - 1) || 1
+                    )
+                })
+        }
+    }
+}
+</script>
+
+<style>
+    table {
+        table-layout: fixed;
+    }
+</style>
